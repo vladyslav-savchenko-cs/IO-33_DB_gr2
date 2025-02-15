331ad20a5a96a6e3351d359aade4c2604c88bc59
vladyslav-savchenko-cs <vladyslav.savchenko@corpsoft.io>
2025-02-15 18:02:41 +0200

    test build

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -5,11 +5,11 @@ export const routes = Object.fromEntries([
   ["/conclusion/", { loader: () => import(/* webpackChunkName: "v-14b1fb01" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/conclusion/index.html.js"), meta: {"y":"a","t":"Висновки"} }],
   ["/design/", { loader: () => import(/* webpackChunkName: "v-b34bd1fc" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/design/index.html.js"), meta: {"y":"a","t":"Проєктування бази даних"} }],
   ["/intro/", { loader: () => import(/* webpackChunkName: "v-1797ca80" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/intro/index.html.js"), meta: {"y":"a","t":"Вступ"} }],
-  ["/software/", { loader: () => import(/* webpackChunkName: "v-254bde0e" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/software/index.html.js"), meta: {"y":"a","t":"Реалізація інформаційного та програмного забезпечення"} }],
-  ["/test/", { loader: () => import(/* webpackChunkName: "v-a35b1ba4" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/test/index.html.js"), meta: {"y":"a","t":"Тестування працездатності системи"} }],
   ["/requirements/", { loader: () => import(/* webpackChunkName: "v-5fd0d570" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/requirements/index.html.js"), meta: {"y":"a","t":"Розроблення загальних вимог до системи"} }],
   ["/requirements/stakeholders-needs.html", { loader: () => import(/* webpackChunkName: "v-53674b13" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/requirements/stakeholders-needs.html.js"), meta: {"y":"a","t":"Запити зацікавлених осіб"} }],
   ["/requirements/state-of-the-art.html", { loader: () => import(/* webpackChunkName: "v-4cbcbf63" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/requirements/state-of-the-art.html.js"), meta: {"y":"a","t":"Аналіз предметної області"} }],
+  ["/software/", { loader: () => import(/* webpackChunkName: "v-254bde0e" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/software/index.html.js"), meta: {"y":"a","t":"Реалізація інформаційного та програмного забезпечення"} }],
+  ["/test/", { loader: () => import(/* webpackChunkName: "v-a35b1ba4" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/test/index.html.js"), meta: {"y":"a","t":"Тестування працездатності системи"} }],
   ["/use%20cases/", { loader: () => import(/* webpackChunkName: "v-44b3e802" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/use cases/index.html.js"), meta: {"y":"a","t":"Модель прецедентів"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "v-8fe89ed2" */"C:/Users/chmo123/Documents/unik/db/docs/.vuepress/.temp/pages/404.html.js"), meta: {"y":"p","t":""} }],
 ]);
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,7 +4,7 @@ import MarkdownItPlantuml from 'markdown-it-plantuml';
 
 export default defineUserConfig({
   // Замінити на назву свого репозиторія
-  base: "/db/",
+  base: "/",
 
   lang: "en-US",
   // Замінити на назву свого проєкту
