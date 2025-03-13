8c1392d67f490e9c7e8de9d130e9b6c73da5088c
neuutaa <zamkovij.vitalik@lll.kpi.ua>
2025-03-13 20:29:05 +0200

    removed vuepress

--- docs/.vuepress/.temp/theme-hope/sidebar.js
+++
@@ -1,1 +0,0 @@
-export const sidebarData = {"/intro/":[""],"/requirements":["","state-of-the-art","stakeholders-needs"],"/use cases/":[""],"/design/":[""],"/software/":[""],"/test/":[""],"/conclusion/":[""]};
